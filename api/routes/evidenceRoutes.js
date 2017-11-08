c08cb6830e4d7063ac9387e16fb349bd41a658ee
Cristian Carreño <cicarreno@uc.cl>
2017-11-08 16:18:46 -0300

    API update

--- api/routes/evidenceRoutes.js
+++ api/routes/evidenceRoutes.js
@@ -1,16 +1,11 @@
-var express = require('express');
-var router = express.Router();
-var evidences = require('../controllers/evidenceController');
+var express = require('express')
+var router = express.Router()
+var evidences = require('../controllers/evidenceController')
 
-/* GET home page. */
-router.get('/', evidences.listAllEvidences);
+router.get('/:evidenceId', evidences.readEvidence)
 
-router.post('/', evidences.createEvidence);
+router.put('/:evidenceId', evidences.updateEvidence)
 
-router.get('/:evidenceId', evidences.readEvidence);
+router.delete('/:evidenceId', evidences.deleteEvidence)
 
-router.put('/:evidenceId', evidences.updateEvidence);
-
-router.delete('/:evidenceId', evidences.deleteEvidence);
-
-module.exports = router;
+module.exports = router
